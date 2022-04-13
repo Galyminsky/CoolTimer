e36e1eb1b67ef5351f0cc01fafa342b4e6b50457
Lisander <jobforandroid@protonmail.com>
2022-04-13 14:20:35 +0600

    Completed

--- app/src/main/java/com/jobforandroid/cooltimer/MainActivity.java
+++ app/src/main/java/com/jobforandroid/cooltimer/MainActivity.java
@@ -1,22 +1,22 @@
 package com.jobforandroid.cooltimer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar seekBar;
-    TextView time;
-    Button btn_start;
+    private SeekBar seekBar;
+    private TextView time;
+    private Button btn_start;
+    private boolean isTimerOn;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,49 +28,85 @@ public class MainActivity extends AppCompatActivity {
         btn_start = findViewById(R.id.btn_start);
 
         seekBar.setMax(500);
-        seekBar.setProgress(50);
+        seekBar.setProgress(30);
+        isTimerOn = false;
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
 
-                int minutes = progress/60;
-                int seconds = progress- (minutes * 60);
-
-                String minutesString = "";
-                String secondsString = "";
-
-                if (minutes < 10) {
-                    minutesString = "0" + minutes;
-                } else {
-                    minutesString = String.valueOf(minutes);
-                }
-
-                if (seconds < 10) {
-                    secondsString = "0" + seconds;
-                }else {
-                    secondsString = String.valueOf(seconds);
-                }
-
-                time.setText(minutesString + ":" + secondsString);
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
+    }
 
+    public void start(View view) {
 
+        if (!isTimerOn) {
+            btn_start.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+
+                @Override
+                public void onTick(long l) {
+                    updateTimer(l);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ring);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            }.start();
+
+        } else {
+            resetTimer();
+        }
 
     }
 
+    private void updateTimer(long l) {
+        int minutes = (int) l / 1000 / 60;
+        int seconds = (int) l / 1000 - (minutes * 60);
 
+        String minutesString = "";
+        String secondsString = "";
+
+        if (minutes < 10) {
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = String.valueOf(minutes);
+        }
+
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = String.valueOf(seconds);
+        }
+
+        time.setText(minutesString + ":" + secondsString);
+    }
+
+    private void resetTimer() {
+        countDownTimer.cancel();
+        time.setText("00:30");
+        btn_start.setText("Start");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(30);
+        isTimerOn = false;
+    }
 
 }
