fce2a3c0ebfb2c2fef1fa53428c52d1050b9d56e
Lisander <jobforandroid@protonmail.com>
2022-04-15 12:14:57 +0600

    setPreferenceLabel

--- app/src/main/java/com/jobforandroid/cooltimer/SettingFragment.java
+++ app/src/main/java/com/jobforandroid/cooltimer/SettingFragment.java
@@ -1,15 +1,46 @@
 package com.jobforandroid.cooltimer;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 
-public class SettingFragment  extends PreferenceFragmentCompat {
+public class SettingFragment extends PreferenceFragmentCompat {
 
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.timer_preferences);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+        int count = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceLabel(preference, value);
+            }
+
+        }
+    }
+
+    private void setPreferenceLabel(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(value);
+            if (index >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[index]);
+            }
+        }
     }
 }
